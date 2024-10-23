1b28b07973cbe61508e21419f561ab1a40aa0e39
Katherine Brown <155707632+Katherine-Brown-8000@users.noreply.github.com>
2024-10-22 22:27:20 -0400

    Create GC calculator (fasta).py

---
+++ GC calculator (fasta).py
@@ -0,0 +1,23 @@
+file_name = r"C:\Users\scien\Downloads\lambda_virus.fa"
+
+def read_file(file_name):
+    with open(file_name, 'r') as file:
+        content = file.readlines()
+        sequence = ''.join([line.strip() for line in content if not line.startswith('>')])
+        return sequence
+
+seq = read_file(file_name)
+
+def gc_calculator(seq):
+    gc_count = 0
+    total_bases = len(seq)
+
+    for base in seq:
+        if base in ("C", "G", "c", 'g'):
+            gc_count += 1
+
+    gc_percent = (gc_count / total_bases) * 100
+    return gc_percent
+
+gc_content = gc_calculator(seq)
+print(gc_content)
